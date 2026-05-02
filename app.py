ba496f6359d8d2d03e0b1d90961b623180644a58
vujjini <vujjiniram143@gmail.com>
2026-05-01 20:02:00 -0400

    empty commit jenkins test

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ def serialize_note(note):
     note["id"] = str(note.pop("_id"))
     return note
 
-
 @app.route("/")
 def index():
     return jsonify({"status": "ok", "message": "Notes & Todo API"})
